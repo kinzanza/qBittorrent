9d874e7807eaeac7a7303e0a4b93db3c9dc7be60
Григорий Гриша <grigoriy.salikov@gmail.com>
2025-07-02 22:55:51 +0500

    мой маааленький тест!

---
+++ test/my_test.cpp
@@ -0,0 +1,13 @@
+#include <QtTest>
+class MyFeatureTest : public QObject
+{
+    Q_OBJECT
+
+private slots:
+    void dummyTest() {
+        QVERIFY(true);
+    }
+};
+
+QTEST_MAIN(MyFeatureTest)
+#include "testmyfeature.moc"
